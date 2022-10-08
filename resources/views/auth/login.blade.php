344aaa0737881f92ef1ff3390689ba870d235323
geitjoch <101874339+u200bpl@users.noreply.github.com>
2022-10-08 16:19:17 +0200

    Added errors to Login and Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,15 @@
 
 <div class="wrapper">
     <div class="create-form">
+        @if($errors->any())
+            <div class="alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
